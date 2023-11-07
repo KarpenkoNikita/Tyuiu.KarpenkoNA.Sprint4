b4c09335ae32edfef56b52ef8f31ad68714fb050
Tyuiu.KarpenkoNA@outlook.com <karpenko.nik05@mail.ru>
2023-11-07 14:52:04 +0500

    Task5

---
+++ Tyuiu.KarpenkoNA.Sprint4.Task5.V23.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KarpenkoNA.Sprint4.Task5.V23.Lib;
+
+namespace Tyuiu.KarpenkoNA.Sprint4.Task5.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] mas2 = new int[5, 5] { { 5, -4, 5, -2, 5 },
+                                          { 5, 5, -3, 8, 4 },
+                                          { 8, -2, 5, 8, -4 },
+                                          { -3, 5, -1, 4, 8 },
+                                          { 8, -5, 6, -2, 8 } };
+
+            int[,] res = ds.Calculate(mas2);
+            int[,] wait = { { 5, 0, 5, 0, 5 },
+                            { 5, 5, 0, 8, 4 },
+                            { 8, 0, 5, 8, 0 },
+                            { 0, 5, 0, 4, 8 },
+                            { 8, 0, 6, 0, 8 } }; 
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KarpenkoNA.Sprint4.Task5.V23/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KarpenkoNA.Sprint4.Task5.V23.Lib;
+
+namespace Tyuiu.KarpenkoNA.Sprint4.Task5.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Random rnd = new Random();
+
+            Console.Title = "Спринт #4 | Выполнил: Карпенко Н.А. | АСОиУБ-23-2";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (генератор случайных чисел)                    *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнил: Карпенко Никита Андреевич |  АСОиУБ-23-2                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* случайными значениями в диапазоне от -4 до 8. Заменить отрицательные    *");
+            Console.WriteLine("* элементы на 0.                                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите количество строк в массиве: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите количество столбцов в массиве: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            int[,] mtrx = new int[rows, columns];
+
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    mtrx[i, j] = rnd.Next(-4, 9);
+                }
+            }
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] res = ds.Calculate(mtrx);
+
+            for(int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{res[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
